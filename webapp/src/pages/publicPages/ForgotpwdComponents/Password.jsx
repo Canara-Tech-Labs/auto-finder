262ffb340de026d5ab8b977692bfd8c33b778675
Vijayalaxmi-R <viju0206@gmail.com>
2023-06-09 10:36:51 +0530

    Corrected conplict files

--- webapp/src/pages/publicPages/ForgotpwdComponents/Password.jsx
+++ webapp/src/pages/publicPages/ForgotpwdComponents/Password.jsx
@@ -1,31 +1,31 @@
-<<<<<<< HEAD
-import React from 'react'
-import './Password.css'
-import { Link } from 'react-router-dom'
 
-export default function Password() {
-  return (
-    <div>
-      <div className='box'>
+// import React from 'react'
+// import './Password.css'
+// import { Link } from 'react-router-dom'
+
+// export default function Password() {
+//   return (
+//     <div>
+//       <div className='box'>
         
       
       
-      <div className='text'>
-        Create New Password
-      </div>
+//       <div className='text'>
+//         Create New Password
+//       </div>
 
-      <div className='New Password'>
-          <input type="text" placeholder='New Password' className='new' /></div>
+//       <div className='New Password'>
+//           <input type="text" placeholder='New Password' className='new' /></div>
 
-          <div className='Confirm Password'>
-          <input type="text" placeholder='Confirm Password' className='confirm' /></div>
+//           <div className='Confirm Password'>
+//           <input type="text" placeholder='Confirm Password' className='confirm' /></div>
 
-          <Link to="/login-page"><button className='sign'>Submit</button></Link>
-          </div>
-    </div>
-  )
-}
-=======
+//           <Link to="/login-page"><button className='sign'>Submit</button></Link>
+//           </div>
+//     </div>
+//   )
+// }
+
 import React, { useState } from 'react';
 import './Password.css';
 import { Link } from 'react-router-dom';
@@ -121,4 +121,4 @@ function Password() {
 }
 
 export default Password;
->>>>>>> c89ee948fbbdf9775b95a5637b384e26219d5eff
+
--- webapp/src/pages/publicPages/RegisterComponents/Register.jsx
+++ webapp/src/pages/publicPages/RegisterComponents/Register.jsx
@@ -1,61 +1,6 @@
 import React from 'react'
 import './Register1.css';
 import { useState } from 'react';
-<<<<<<< HEAD
-
-const Register = () => {
-
-  const[Firstname, setFirstname] = useState('');
-  const[Lastname, setLastname] = useState('');
-  const[Email, setEmail] = useState('');
-  const[Password, setPassword] = useState('');
-  const[errors, setErrors]= useState({});
-
-  const handleSubmit=(e) =>{
-    e.preventDefault();
-
-    setErrors({});
-
-    let isValid=true;
-    const newErrors={};
-
-    if (Firstname.trim() == ''){
-      newErrors.Firstname = 'First Name is required';
-      isValid=false;
-    }
-
-    if (Lastname.trim() == ''){
-      newErrors.Lastname = 'Last Name is required';
-      isValid=false;
-    }
-
-    if (Email.trim() == ''){
-      newErrors.Email = 'Email is required';
-      isValid=false;
-    }
-
-    if (Password.trim() == ''){
-      newErrors.Password = 'Password is required';
-      isValid=false;
-    }
-
-    if (!isValid){
-      setErrors(newErrors);
-      return;
-    }
-
-    setFirstname('');
-    setLastname('');
-    setEmail('');
-    setPassword('');
-
-  }
-
-
-  return (
-    <form onSubmit={handleSubmit}>
-    <div>
-=======
 import { Link } from 'react-router-dom';
 
 function Register () {
@@ -147,20 +92,15 @@ function Register () {
   return (
   <form>
     <div className='register'>
->>>>>>> c89ee948fbbdf9775b95a5637b384e26219d5eff
       <div className='form'>
         <div className='text'>
-            <div className='text1'>Welcome to the Autoservice</div>
-            <div className='text2'>Let's get your all set up, so you can being tomake your first service</div>
+            <div className='text1'>REGISTRATION</div>
+            <div className='text2'>Enter your personal data</div>
       
         </div>
 
         <div className='name'>
             First Name
-<<<<<<< HEAD
-        <input type="text" placeholder='First Name'className='name1' onChange={(e)=> setFirstname(e.target.value)}/>
-        {errors.Firstname && <div className='error' style={{ color: 'red' }}>{errors.Firstname}</div>}
-=======
         <input 
         type="text" 
         name='firstname'
@@ -170,15 +110,10 @@ function Register () {
         onChange={onInputChange} 
         onBlur={validateInput}></input>
         {error.firstname && <span className='err'>{error.firstname}</span>}
->>>>>>> c89ee948fbbdf9775b95a5637b384e26219d5eff
         </div>
 
         <div className='name-1'>
             Last Name
-<<<<<<< HEAD
-        <input type="text" placeholder='Last Name'className='name2' onChange={(e)=> setLastname(e.target.value)}/>
-        {errors.Lastname && <div className='error'  style={{ color: 'red' }}>{errors.Lastname}</div>}
-=======
         <input 
         type="text" 
         name='lastname'
@@ -189,15 +124,10 @@ function Register () {
         onBlur={validateInput}></input>
         {error.lastname && <span className='err'>{error.lastname}</span>}
 
->>>>>>> c89ee948fbbdf9775b95a5637b384e26219d5eff
         </div>
 
         <div className='email'>
             Email
-<<<<<<< HEAD
-        <input type="text" placeholder='Email'className='email1'  onChange={(e)=> setEmail(e.target.value)}/>
-        {errors.Email && <div className='error' style={{ color: 'red' }}>{errors.Email}</div>}
-=======
         <input type="text" 
         name='email'
         placeholder='Email'
@@ -207,15 +137,10 @@ function Register () {
         onBlur={validateInput}></input>
          {error.email && <span className='err'>{error.email}</span>}
 
->>>>>>> c89ee948fbbdf9775b95a5637b384e26219d5eff
         </div>
 
         <div className='password'>
             Password
-<<<<<<< HEAD
-        <input type="text" placeholder='Password'className='password1'  onChange={(e)=> setPassword(e.target.value)}/>
-        {errors.Password && <div className='error' style={{ color: 'red' }}>{errors.Password}</div>}
-=======
         <input type="text" 
         name='password'
         placeholder='Password'
@@ -226,23 +151,13 @@ function Register () {
         {error.password && <span className='err'>{error.password}</span>}
         
 
->>>>>>> c89ee948fbbdf9775b95a5637b384e26219d5eff
         </div>
 
         <div className='confirm-password'>
             Confirm Password
-<<<<<<< HEAD
-        <input type="text" placeholder='confirm Password'className='confirm'/>
-        </div>
-
-        <div className='submit'>
-            <button className='sub'>Submit</button>
-        </div>
-
-=======
         <input type="text"
         name='confirmpassword'
-         placeholder='confirm Password'
+         placeholder='Confirm Password'
          className='confirm'
          value={input.confirmpassword} 
         onChange={onInputChange} 
@@ -254,7 +169,6 @@ function Register () {
           <Link to={"/login-page"} > <button className='sub'>Submit</button></Link>
         </div>
         
->>>>>>> c89ee948fbbdf9775b95a5637b384e26219d5eff
 
       </div>
     </div>
